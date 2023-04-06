07de9095a4b5080b558fbc61e335e96a75fb683f
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-04-06 20:25:23 +0530

    final commit

---
+++ pages/profiles.tsx
@@ -0,0 +1,37 @@
+import { NextPageContext } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
+
+const Profiles = () => {
+    const { data: user } = useCurrentUser();
+
+    return (
+        <div>
+            <h1>Profiles</h1>
+        </div>
+    )
+}
+
+export default Profiles
